924479c941e0a751f4344c549c4eb191a9bcc778
Matt Bailey <you@example.com>
2021-05-21 15:18:47 +0100

    add app js

---
+++ app/hooli/app.js
@@ -0,0 +1,16 @@
+const express = require('express');                                                                                        
+const bodyParser = require('body-parser')                                                                                  
+const app = express();                                                                                                     
+const listenPort = 8080;                                                                                                   
+                                                                                                                           
+app.use(bodyParser.urlencoded({ extended: false }))                                                                        
+app.use(bodyParser.json())                                                                                                 
+                                                                                                                           
+// Ping request                                                                                                            
+app.get('/ping',  (req,res,next) => {                                                                                      
+                                                                                                                           
+    res.send({"result": "pong"})                                                                                           
+                                                                                                                           
+});                                                                                                                        
+                                                                                                                           
+app.listen(listenPort, () => console.log("Server is Running on " + listenPort)); 
